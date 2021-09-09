408587af4b154bfd8591c88e445aa1fe638b9702
jjlevy <jjlevy@crimson.ua.edu>
2021-09-09 12:32:07 -0500

    fixed spacing on row/col

--- src/MapChart.js
+++ src/MapChart.js
@@ -45,8 +45,12 @@ const MapChart = ({ setTooltipContent }) => {
 
   return (
     <>
-      <Row>
-        <Col md={10} style={{ outlineColor: "red", outline: "1px" }}>
+      <Row className="layoutRow">
+        <Col
+          md={10}
+          style={{ outlineColor: "red", outline: "1px" }}
+          className="layoutColumn"
+        >
           <MapArea
             setTooltipContent={setTooltipContent}
             low={low}
@@ -56,40 +60,42 @@ const MapChart = ({ setTooltipContent }) => {
             stateData={stateData}
           />
         </Col>
-        <Col md={2}>
+        <Col
+          md={2}
+          style={{ backgroundColor: "lightgrey" }}
+          className="layoutColumn"
+        >
           <h2 style={{ textAlign: "center" }}>{year}</h2>
 
           <SelectYear setYear={setYear} />
           <SetRange low={low} high={high} setLow={setLow} setHigh={setHigh} />
-          <Row>
-            <button onClick={handleZoomIn}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="3"
-              >
-                <line x1="12" y1="5" x2="12" y2="19" />
-                <line x1="5" y1="12" x2="19" y2="12" />
-              </svg>
-            </button>
-          </Row>
-          <Row>
-            <button onClick={handleZoomOut}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="3"
-              >
-                <line x1="5" y1="12" x2="19" y2="12" />
-              </svg>
-            </button>
-          </Row>
+
+          <button onClick={handleZoomIn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
+
+          <button onClick={handleZoomOut}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
         </Col>
       </Row>
     </>
